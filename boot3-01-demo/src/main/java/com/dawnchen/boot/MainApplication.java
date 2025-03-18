505338cedac3a4fed91249ed84e234d8608307ba
mornchen <win.hydrogen@gmail.com>
2025-03-18 12:20:26 +0800

    更改

--- boot3-01-demo/src/main/java/com/dawnchen/boot/MainApplication.java
+++ boot3-01-demo/src/main/java/com/dawnchen/boot/MainApplication.java
@@ -1,13 +1,20 @@
 package com.dawnchen.boot;
 
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
 *@BelongsProject: Learn-SpringBoot3
 *@BelongsPackage: com.dawnchen.boot
 *@Author: dawnchen
 *@CreateTime: 2025-03-18  11:19
-*@Description: TODO
+*@Description: 启动SpringBoot项目的主入口程序
 */
 
+@SpringBootApplication //这是一个SpringBoot应用
 public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
 }
--- boot3-01-demo/src/main/java/com/dawnchen/boot/controller/HelloController.java
+++ boot3-01-demo/src/main/java/com/dawnchen/boot/controller/HelloController.java
@@ -1,13 +1,23 @@
 package com.dawnchen.boot.controller;
 
 
-/**
-*@BelongsProject: Learn-SpringBoot3
-*@BelongsPackage: com.dawnchen.boot.controller
-*@Author: dawnchen
-*@CreateTime: 2025-03-18  11:36
-*@Description: TODO
-*/
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @BelongsProject: Learn-SpringBoot3
+ * @BelongsPackage: com.dawnchen.boot.controller
+ * @Author: dawnchen
+ * @CreateTime: 2025-03-18  11:36
+ * @Description: TODO
+ */
+
+//@ResponseBody
+//@Controller
+@RestController
 public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, Spring Boot 3!";
+    }
 }
