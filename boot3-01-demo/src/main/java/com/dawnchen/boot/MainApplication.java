f16b6c78085765c112592674e94812e1a47bfca8
mornchen <win.hydrogen@gmail.com>
2025-03-18 11:45:35 +0800

    Initial commit

---
+++ boot3-01-demo/src/main/java/com/dawnchen/boot/MainApplication.java
@@ -0,0 +1,13 @@
+package com.dawnchen.boot;
+
+
+/**
+*@BelongsProject: Learn-SpringBoot3
+*@BelongsPackage: com.dawnchen.boot
+*@Author: dawnchen
+*@CreateTime: 2025-03-18  11:19
+*@Description: TODO
+*/
+
+public class MainApplication {
+}
---
+++ boot3-01-demo/src/main/java/com/dawnchen/boot/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.dawnchen.boot.controller;
+
+
+/**
+*@BelongsProject: Learn-SpringBoot3
+*@BelongsPackage: com.dawnchen.boot.controller
+*@Author: dawnchen
+*@CreateTime: 2025-03-18  11:36
+*@Description: TODO
+*/
+
+public class HelloController {
+}
